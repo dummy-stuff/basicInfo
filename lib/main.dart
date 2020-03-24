4dfad74c7067350d48e0f269a0c2f2f47c073366
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-03-24 16:59:12 +0100

    Change app name from myinfo to basicInfo

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final theStyle = TextStyle(fontSize:18);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Basic Info")),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            ListTile(title: Text("Name"), trailing: Text("Chukwujama Osinachi Victor")),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Basic Info")),
+        body: Container(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text("Full Name", style: theStyle,),
+                trailing: Text("Chukwujama Osinachi Victor", style: theStyle,),
+              ),
+              ListTile(
+                title: Text("Slack Username", style: theStyle,),
+                trailing: Text("Osinachi", style: theStyle,),
+              ),
+              ListTile(
+                title: Text("Start.ng Email Address", style: theStyle,),
+                trailing: Text("vicraph7@gmail.com", style: theStyle,),
+              ),
+              ListTile(
+                title: Text("My Tracks", style: theStyle,),
+                trailing: Text("Backend, Mobile", style: theStyle,),
+              ),
+            ],
+          ),
         ),
       ),
     );
